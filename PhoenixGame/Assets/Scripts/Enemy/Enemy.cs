bffe5728cd710765f8b5e3f49ab5371567841a34
ChristopherHernandez117 <ics20-020@camosun.bc.ca>
2022-04-04 20:54:01 -0700

    Enemy states and death added
    The enemy can now die if hit with sword multiple times

--- PhoenixGame/Assets/Scripts/Enemy/Enemy.cs
+++ PhoenixGame/Assets/Scripts/Enemy/Enemy.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject Player { get; private set; }
+    [SerializeField] public NavMeshAgent agent;
+    public float ChaseRange { get; private set; } = 10.0f;
     private int lifePoints = 5;
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Sword")
         {
-            Debug.Log("Hit with sword");
+            lifePoints--;
+            Debug.Log("Life points:" + lifePoints);
         }
+        if(lifePoints <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public float GetDistanceFromPlayer()
+    {
+        return Vector3.Distance(transform.position, Player.transform.position);
     }
 }
